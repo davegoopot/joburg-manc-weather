fcd7f09380095d1f72ea1cd4be0debf73dbfe7f8
Dave Potts <dave@goopot.co.uk>
2021-07-31 18:30:47 +0000

    Initial simple test page with static temperature data.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace joburg_manc_weather.Pages
+{
+    public record WeatherData
+    {
+        public decimal MancTempC;
+        public decimal JoburgTempC;
+    }
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["weather"] = new WeatherData
+            {
+                MancTempC = 17.5M,
+                JoburgTempC = 24.1M
+            };
+
+        }
+    }
+}
